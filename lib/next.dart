824f03e0876479ec5ca9f3e10262d7fbb75912e7
Aung-Zay-Thant <aungzaythant.csdt@gmail.com>
2020-08-23 15:51:17 +0630

    Aug 23

--- lib/next.dart
+++ lib/next.dart
@@ -9,18 +9,106 @@ class _NextScreenState extends State<NextScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "This is second screen",
-          style: TextStyle(color: Colors.blue, fontSize: 20),
-        ),
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //Back to previous screen
-          Navigator.pop(context);
-        },
-        child: Icon(Icons.backspace),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              // color: Colors.black12,
+              child: Row(children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GestureDetector(
+                      onTap: () => print("Top Box 1"),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: GestureDetector(
+                    onTap: () => print("Top Box 2"),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue[100],
+                      ),
+                    ),
+                  ),
+                ))
+              ]),
+            ),
+          ),
+          Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GestureDetector(
+                  onTap: () => print("Middle box"),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.yellowAccent),
+                  ),
+                ),
+              )),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.black38,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: GestureDetector(
+                        onTap: () => print("Buttom box1"),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Colors.yellow,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GestureDetector(
+                      onTap: () => print('Buttom box2'),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ))
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Container(
+                  color: Colors.redAccent,
+                ),
+              ))
+        ],
       ),
     );
   }
